258c035cfa0691d5348507a73da5064902c3061a
Paul Laskowski <pelaskowski@gmail.com>
2014-02-22 21:58:39 -0800

    Created scopes by office_id on the employees table. (plus a minor edit in views/employees/_form_html.erb)

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,13 @@ class Employee < ActiveRecord::Base
   has_many :statuses
   belongs_to :office
   
+  def self.nyc 
+    where(default_office_id: '1')
+  end
+  
+  def self.seattle
+    where(default_office_id: '2')
+  end
+  
+  
 end
