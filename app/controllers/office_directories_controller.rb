2490a20ff0d04e88730cac59c276f1eaa147ef56
Paul Laskowski <pelaskowski@gmail.com>
2014-03-09 23:03:05 -0700

    made some style changes

--- app/controllers/office_directories_controller.rb
+++ app/controllers/office_directories_controller.rb
@@ -1,8 +1,8 @@
 class OfficeDirectoriesController < ApplicationController
   
   def index
-    @nyc_full_names = Employee.nyc.pluck(:first_name, :last_name)
-    @seattle_full_names = Employee.seattle.pluck(:first_name, :last_name)
+    @nyc_full_names = Employee.nyc.pluck(:title, :first_name, :last_name)
+    @seattle_full_names = Employee.seattle.pluck(:title, :first_name, :last_name)
   end
   
 end
