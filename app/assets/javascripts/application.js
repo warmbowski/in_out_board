d09896ddf5258c21a83f7411db5bf91a434ae0c2
Paul Laskowski <pelaskowski@gmail.com>
2014-03-12 18:42:36 -0700

    Added jquery-ui-rails gem installed and configured. Set up the intial bits of dragging cards dragging between in/out lists.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
