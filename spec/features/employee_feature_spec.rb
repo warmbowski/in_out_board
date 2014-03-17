bf0fb75660ea93839eae6cc89bd685d32b760e53
Paul Laskowski <pelaskowski@gmail.com>
2014-03-17 16:43:38 -0700

    added a couple of integration tests (they're not working).

--- spec/features/employee_feature_spec.rb
+++ spec/features/employee_feature_spec.rb
@@ -1,1 +1,25 @@
 require 'spec_helper'
+
+
+feature "Default home page" do
+  scenario "viewing the directory page" do
+    visit root_path
+    expect(page).to have_text("In the Office")
+  end
+end
+
+describe Employee, type: :feature do
+  
+  context "Creating new employee" do
+    
+    it "should let the user know about missing first name" do
+      
+      visit '/employee/new'
+      click_button "Create Employee"
+      page.should have_content "First name can't be blank"
+      
+    end
+    
+  end
+  
+end
