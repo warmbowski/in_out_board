9db587b2664b130d12de8f49f9d373abc99a83d4
Paul Laskowski <pelaskowski@gmail.com>
2014-03-30 22:23:17 -0700

    Add Mailtrap addon setting to the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ InOutBoard::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  if ENV['MAILTRAP_HOST'].present?
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['MAILTRAP_USER_NAME'],
+      :password => ENV['MAILTRAP_PASSWORD'],
+      :address => ENV['MAILTRAP_HOST'],
+      :port => ENV['MAILTRAP_PORT'],
+      :authentication => :plain
+    }
+  end
 end
